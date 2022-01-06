5c5abed1b9863e26b77dbbc94890f8d0a699fab4
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2022-01-06 15:40:16 -0600

    build mortgage calcultor

--- src/com/jeff/Main.java
+++ src/com/jeff/Main.java
@@ -1,8 +1,34 @@
 package com.jeff;
 
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       final byte MONTHS_IN_YEAR = 12;
+       final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("annual interest rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (years):");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
